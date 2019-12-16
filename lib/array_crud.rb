32de51050078a59b7eae883bd2cfb738107dd59f
Joshua Sanders <ninjosh2@gmail.com>
2019-12-16 08:44:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Wow", "I", "am", "really", "learning"]
-  array << element
+  array.push element
 end
 
 def add_element_to_start_of_array(array, element)
